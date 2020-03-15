87de13594576ab2355c4cce7512326e763b03331
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 19:11:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(array) 
+  if array.count == 0 
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently: "
+    array.each_with_index do |name, index|
+      
+  end 
+end
