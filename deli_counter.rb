5f84adc06957a07678cd008646dabe674215060b
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 19:16:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.count == 0 
     puts "The line is currently empty."
   else 
-    say =  "The line is currently: "
+    say =  "The line is currently:"
     array.each_with_index do |name, index|
       say += " #{index + 1}. #{name}"
   end 
