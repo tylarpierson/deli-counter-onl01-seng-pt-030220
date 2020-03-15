4d159a37ae808656a7e6c5f20b386c3120af3cec
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 19:20:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,12 @@ def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array, name)
+  array.pop(name)
+  if array.count == 0 
+    puts "There is noby waiting to be served!"
+  else
+    puts "now serving #{name}"
+  end
+end
