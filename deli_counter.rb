1f808273f1d2cd1b7587fb4a9c4d8363233842e1
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 19:23:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ def now_serving(array)
   if array.count == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array.shift}"
+    puts "Currently serving #{array.shift}."
   end
 end
