793f35234c9cff46db6bffad07e6b7b3c94d7efa
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 19:18:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(array, name) 
   array.push(name)
-  puts "Welcome #{name} you are #{array.length} in line."
+  puts "Welcome, #{name}. You are number  #{array.length} in line."
 end
